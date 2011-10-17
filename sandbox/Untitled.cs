0c5778da41260dc79950d577df1897839a147764
root <root@rails.(none)>
2011-10-17 21:27:47 +0000

    8

--- sandbox/Untitled.cs
+++ sandbox/Untitled.cs
@@ -22,7 +22,27 @@ public class LifeCalculator
 
     private static bool CalculateCell(string[] input, int row, int col)
     {
-        return true;
+        bool isAlive = input[row][col] == '*';
+        int neighbors = 0;
+        neighbors = CountNeighbors(input, row, col);
+        // rule 1:
+        if (isAlive && neighbors < 2)
+            return false;
+        // rule 2:
+        if (isAlive && neighbors > 3)
+            return false;
+        // rule 3:
+        if (isAlive && neighbors >= 2 && neighbors <=3)
+            return true;
+        // rule 4:
+        if (!isAlive && neighbors == 3)
+            return true;
+        return false;
+    }
+
+    private static int CountNeighbors(string[] input, int row, int col)
+    {
+        return 3;
     }
 
     private static string[] CreateEmptyClone(string[] source)
