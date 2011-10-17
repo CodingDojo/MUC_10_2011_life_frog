acce08eaa6c690d9302097d277d6b4f8017ae2e7
root <root@rails.(none)>
2011-10-17 22:13:51 +0000

    29

--- sandbox/UntitledTest.cs
+++ sandbox/UntitledTest.cs
@@ -72,5 +72,20 @@ public class UntitledTest
         Assert.AreEqual("*.*",result[2], "Third Row failed.");
     }
 
+    [Test]
+    public void Empty()
+    {
+        var result = LifeCalculator.Calc(new string[]{ });
+        Assert.AreEqual(0, result.Length);
+    }
+    [Test]
+    public void Empty()
+    {
+        var result = LifeCalculator.Calc(new string[]{"",""});
+        Assert.AreEqual(1, result.Length);
+        Assert.AreEqual("", result[0]);
+        Assert.AreEqual("", result[1]);
+    }
+
 }
 
