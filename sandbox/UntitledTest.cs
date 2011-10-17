76e0ff5d1e14364b3e3ba6d9542f0dfa9c318d3c
root <root@rails.(none)>
2011-10-17 21:31:04 +0000

    12

--- sandbox/UntitledTest.cs
+++ sandbox/UntitledTest.cs
@@ -12,6 +12,23 @@ public class UntitledTest
         Assert.AreEqual(".",result[0]);
     }
 
+    [Test]
+    public void Simple3x1ToLife()
+    {
+        var result = LifeCalculator.Calc(new string[]{".*.");
+        Assert.AreEqual(1, result.Length);
+        Assert.AreEqual("...",result[0]);
+    }
+
+//    [Test]
+    public void Complex2x2ToLife()
+    {
+        var result = LifeCalculator.Calc(new string[]{".*",
+                                       "**"});
+        Assert.AreEqual(2, result.Length);
+        Assert.AreEqual("**",result[0]);
+        Assert.AreEqual("**",result[0]);
+    }
 
 }
 
